b9db40e462b3be675abe09b83bba76726a2dd293
Dmitry1983 <barbash.dv@gmail.com>
2020-06-03 09:50:10 +0300

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,11 @@
+// prettier-ignore
 module.exports = {
 	root: true,
 	extends: '@react-native-community',
 	rules: {
 		'no-unused-vars': 0,
 		'no-extra-semi': 0,
-		semi: 0,
+		'semi': 0,
 		'react-native/no-inline-styles': 0,
 	},
 }
